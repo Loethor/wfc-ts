3f00ccfc12206f10b23169ae2b012002754a5113
loethor <lrb2392@gmail.com>
2025-12-15 11:46:22 +0100

    fix workflow bug

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Set base to your repo name for GitHub Pages
   base: process.env.NODE_ENV === 'production' ? '/wfc-ts/' : '/',
-  resolve: {
-    extensions: ['.ts', '.js', '.json'],
-  },
   build: {
     outDir: 'docs',
     assetsDir: 'assets',
